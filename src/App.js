ecc1a8f4deca5659a1877612c927f5417f314a64
teceudesenvolvo <tecnologia@eudesenvolvo.com>
2025-08-28 09:29:40 -0300

    orgizanização-css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import './AdminSE.css'
+import './AdminSM.css'
 
 // Componentes Fixos
 import SideMenu from './componentes/SideMenu';
